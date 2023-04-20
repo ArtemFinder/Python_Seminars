ee8d7564ec38c3cba02563cf063f1d845ea8a812
Artem <logik562@ya.ru>
2023-04-21 05:59:54 +0700

    lesson 8

--- task32.py
+++ task32.py
@@ -1,1 +1,20 @@
 """Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)"""
+
+from random import randint
+
+
+def createArray(size):
+    list_1 = [randint(-5, 9) for _ in range(size)]
+    return list_1
+
+
+def index(my_list):
+    min_elem = int(input("Введите с какого значения искать: "))
+    max_elem = int(input("Введите по какое значение искать: "))
+    print([i for i in range(len(my_list)) if min_elem <= my_list[i] <= max_elem])
+
+
+num_size = int(input("Введите длину массива: "))
+my_list = createArray(num_size)
+print(my_list)
+index(my_list)
